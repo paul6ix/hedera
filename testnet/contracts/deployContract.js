47c345575e1abaf6af185e8999c799bf53828ea9
paul6ix <okporp@gmail.com>
2023-09-10 17:17:44 +0100

    added contract query

--- testnet/contracts/deployContract.js
+++ testnet/contracts/deployContract.js
@@ -6,7 +6,11 @@ import {
     FileCreateTransaction,
     AccountBalanceQuery,
     AccountInfoQuery,
-    TransferTransaction, ContractCreateTransaction, ContractFunctionParameters, ContractCallQuery,
+    TransferTransaction,
+    ContractCreateTransaction,
+    ContractFunctionParameters,
+    ContractCallQuery,
+    ContractExecuteTransaction,
 } from "@hashgraph/sdk";
 import "dotenv/config";
 import * as fs from 'fs'
@@ -17,6 +21,7 @@ client = Client.forName(process.env.HEDERA_NETWORK).setOperator(
     operatorId,operatorKey);
 
 async function main() {
+    //Adding contract file to the network
     const contractByteCode = fs.readFileSync('testnet/contracts/lookUpContract_sol_LookupContract.bin')
     const  fileCreateTx = new  FileCreateTransaction()
         .setContents(contractByteCode)
@@ -28,7 +33,7 @@ async function main() {
     const fileCreateRX = await fileCreateSubmit.getReceipt(client)
     const byteCodeID = fileCreateRX.fileId
     console.log(`The bytecode file ID is: ${byteCodeID}`)
-
+//contract deployment
     const contractInitiateTX = new ContractCreateTransaction()
         .setBytecodeFileId(byteCodeID)
         .setGas(100000)
@@ -40,7 +45,7 @@ async function main() {
     console.log(`- The contract ID is: ${contractID}`)
     console.log(` The smart contract ID in solidity is: ${contractAddress} `)
 
-
+//contract call
     const contractQueryTX = new ContractCallQuery()
         .setContractId(contractID)
         .setGas(100000)
@@ -49,8 +54,27 @@ async function main() {
 
     const contractQuerySubmit = await contractQueryTX.execute(client);
     const contractQueryResult = await  contractQuerySubmit.getUint256(0);
-    console.log(`Contract call function details: ${contractQueryResult}`)
+    console.log(`Contract call details: ${contractQueryResult}`)
 
+//contract write
+    const contractExecuteTX = new ContractExecuteTransaction()
+        .setContractId(contractID)
+        .setGas(100000)
+        .setFunction("setMobileNumber", new ContractFunctionParameters().addString('Arkhia').addUint256(2222))
+        .setMaxTransactionFee(new Hbar(0.75))
+    const contractExecuteSubmit =  await  contractExecuteTX.execute(client)
+    const  contractExecuteRX = await contractExecuteSubmit.getReceipt(client)
+    console.log(`Contract write successful: ${contractExecuteRX.status}`)
+
+    const contractQueryTX1 = new ContractCallQuery()
+        .setContractId(contractID)
+        .setGas(100000)
+        .setFunction("getMobileNumber", new ContractFunctionParameters().addString("Arkhia"))
+        .setMaxQueryPayment(new Hbar(1));
+
+    const contractQuerySubmit1 = await contractQueryTX1.execute(client);
+    const contractQueryResult1 = await  contractQuerySubmit1.getUint256(0);
+    console.log(`Contract call details: ${contractQueryResult1}`)
 
 
 
